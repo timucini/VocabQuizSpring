f159ebef6ff44684f6354dca19cf9b695576202b
Seweryn <seko1702@gmail.com>
2021-06-09 20:16:07 +0200

    update frontend

--- web/src/main/client/src/components/Round.js
+++ web/src/main/client/src/components/Round.js
@@ -4,10 +4,54 @@ import axios from "axios";
 
 function Round(props) {
 
+
+    // this.state = {questionNumber: 0};
+
+    const [questionNumber, setQuestionNumber] = useState(0);
+    const [currentQuestion, setQuestion] = useState([]);
+    const [lastAnswerValidation, setLastAnswerValidation] = useState(true);
+    // let state;
+    // state = { testVarible: "this is a test" };
+
+
+    // onMove = () => {
+    //     console.log(this.state.testVarible);
+    //     this.setState({ testVarible: "new value" });
+    // }
+
+
+    const submitAnswer = (answer, question) => {
+        axios.get("http://localhost:8080/api/v1/match/answer",
+            { params: { answer: answer, question_id: question.id, match_id: props.match.id, user: props.user.id }}).then(response => {
+            console.log("res", response.data);
+            setLastAnswerValidation(response.data);
+            // setRound(response.data);
+            // setPicking(false);
+        }, (error) => {
+            console.log(console.log(error));
+        });
+        console.log("x");
+    }
+
+    const nextQuestion = () => {
+        // console.log(this.questionNumber);
+        // this.setState( this.questionNumber = this.questionNumber+1);
+        // console.log(this.questionNumber);
+    }
+
+
     return(
         <div>
            <p>IN Round</p>
            <p>Round for category: {props.round.category.name}</p>
+            <p>Question Number {questionNumber+1} / 3</p>
+            <p>Question: translate "{props.round.questions[questionNumber].questionString}"</p>
+            {<button onClick={() => submitAnswer(props.round.questions[questionNumber].correctAnswer, props.round.questions[questionNumber])}> {props.round.questions[questionNumber].correctAnswer} </button>}
+            {<button onClick={() => submitAnswer(props.round.questions[questionNumber].wrongAnswer1, props.round.questions[questionNumber])}> {props.round.questions[questionNumber].wrongAnswer1} </button>}
+            {<button onClick={() => submitAnswer(props.round.questions[questionNumber].wrongAnswer2, props.round.questions[questionNumber])}> {props.round.questions[questionNumber].wrongAnswer2} </button>}
+            {<button onClick={() => submitAnswer(props.round.questions[questionNumber].wrongAnswer3, props.round.questions[questionNumber])}> {props.round.questions[questionNumber].wrongAnswer3} </button>}
+            <p>Your answer was {lastAnswerValidation.toString()} </p>
+            <p>{<button onClick={() => setQuestionNumber(questionNumber+1)}> next Question </button>}</p>
         </div>
     );
 }
